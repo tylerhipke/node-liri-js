7e61bec0331fde5015c398d8d6de861bca43c0dc
tylerhipke <tyerhipke@gmail.com>
2019-08-31 21:23:50 -0700

    add concertsintown axios

--- liri.js
+++ liri.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
+var axios = require('axios');
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
-var userSpecfied = process.argv[3];
+var userSpecfied = "";
 
-switch ( command ){
+var cliAmount = parseInt(process.argv.length);
+
+for (var i = 3; i < cliAmount; i++) {
+    userSpecfied += process.argv[i] + " ";
+}
+userSpecfied = userSpecfied.slice(0, -1);
+
+switch (command) {
     case "concert-this":
         concert();
         break;
@@ -26,6 +34,19 @@ switch ( command ){
 }
 
 function concert() {
+    console.log("Loading concert data for '" + userSpecfied + ".'");
+
+    var url = "https://rest.bandsintown.com/artists/" + userSpecfied + "/events?app_id=codingbootcamp";
+    axios.get(url)
+        .then(function (response) {
+            var thisKey = response.data;
+            for (var i = 0; i < thisKey.length; i++) {
+                console.log("Venue " + (i + 1) + ":\n" +
+                    thisKey[i].venue.name + " in " + thisKey[i].venue.city + ", " + 
+                    thisKey[i].venue.region + "\n");
+
+            }
+        });
 
 }
 
@@ -38,7 +59,7 @@ function movie() {
 
 }
 
-function doThis(){
+function doThis() {
 
 }
 
